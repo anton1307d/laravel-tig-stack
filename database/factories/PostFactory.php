01e660f5735fbd4dbfa9dc16c4a071c5cf23bf83
antonzubb1307d <zubilevych@convidera.com>
2023-05-09 13:33:30 +0300

    fix

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,6 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => fake()->name(),
             'content' => fake()->text,
         ];
     }
